3c7d50e50706a7075e61deac75b4d07c7d208b22
lourity <96919081+lourityy@users.noreply.github.com>
2023-11-15 16:55:15 +0300

    Delete anonim_chat directory

--- anonim_chat/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    token: "", // botun tokeni
-    prefix: ".", // prefixi
-    isim_emoji: "", // isim ve yaşın arasına gelecek emoji
-    anonim_chat_kategori: "",
-    rapor_channel: "",
-    anonim_log: "",
-    anonim_yetkili: ""
-}
